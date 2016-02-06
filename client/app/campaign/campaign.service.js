8005c9b6964057a54519894b8d006d1b93f96b15
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2016-02-06 13:27:28 -0800

    campaign services created

---
+++ client/app/campaign/campaign.service.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+
+	class CampaignService {
+    constructor($http, Auth) {
+      this.http = $http;
+      this.auth = Auth;
+    }
+    getCampaigns(params) {
+      return this.http({
+          method: 'GET',
+          url: '/api/campaigns',
+          params: params
+        })
+        .success(result => result.data);
+    }
+    getCampaign(campaign) {
+      return this.http.get(`/api/campaigns/${campaign}`).success(result => result.data);
+    }
+    createCampaign(form) {
+      var user = this.auth.getCurrentUser()._id;
+      var data = _.extend(form,{ 'user_id': user});
+      if (this.Auth.isLoggedIn() && user) {
+        return this.http.post('/api/campaigns', data);
+      }
+    }
+    updateCampaign(campaign, data) {
+      var user = this.auth.getCurrentUser()._id;
+      if (this.Auth.isLoggedIn() && this.campaign.user_id === user) {
+        return this.http.put(`/api/campaigns/${campaign}`, data);
+      }
+    }
+    updateDonations(campaign) {
+      return this.http.get(`/api/campaigns/${campaign}/contributors`).success(result => result.data);
+    }
+  }
+  
+	angular.module('rnangularApp')
+	  .service('campaign', CampaignService);
+})();
+
