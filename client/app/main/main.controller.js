8f59b909695b11ff9e4eb6eabdc616edabad80b8
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2016-02-06 23:26:15 -0800

    updates to main home page and use of navigator to get location

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,26 +4,46 @@
 
 class MainController {
 
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
+  constructor(geolocation, campaign) {
+    this.geolocation = geolocation;
+    this.campaign = campaign;
+    this.offset = 1;
+    this.geolocation.getIpInfo()
+      .then(data => this.currentLoc = `${data.city}, ${data.region} ${data.postal}`);
+    this.geolocation.getCurrentPosition()
+      .then(data => { this.loc = [data.coords.longitude, data.coords.latitude] });
+      //.then(data => this.loc = data)
+      //.then(() => this.getCampaignResults());
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
+  getCampaignResults(distance, limit) {
+    var self = this;
+    var params = {
+      longitude: self.loc[0],
+      latitude: self.loc[1],
+    };
+    if (distance) params.extend({distance: distance});
+    if (limit) params.extend({limit: limit});
+    this.campaing.getCampaigns(params)
+      .success(data => {
+        self.campaigns = data;
+        self.offset += 1;
+      })
+      .error( err => console.error(`Error: ${err}`));
   }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
+  sortCampaignsBy(type) {
+
   }
+
+  calculateDonations() {
+
+  }
+
 }
+
+MainController.$inject = ['geolocation', 'campaign'];
 
 angular.module('rnangularApp')
   .controller('MainController', MainController);
