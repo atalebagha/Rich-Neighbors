9f3005b6301bd38fdb4498b15cca71dff6f21329
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2016-02-07 13:52:41 -0800

    geolocation at main controller

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,18 +6,16 @@ class MainController {
 
   constructor(geolocation, campaign) {
     this.geolocation = geolocation;
-    this.campaign = campaign;
+    this.campaignSvc = campaign;
+
     this.offset = 1;
     this.geolocation.getIpInfo()
       .then(data => this.currentLoc = `${data.city}, ${data.region} ${data.postal}`)
     //this.getLocation()
     this.geolocation.getCurrentPosition()
       .then(data => this.position = data.coords)
-      .then(data => console.log(this.position))
       .then(data => this.geolocation.getAddress(Math.round(this.position.latitude*1000000)/1000000, Math.round(this.position.longitude*1000000)/1000000))
-      .then(data => console.log(data));
-      //.then(data => this.loc = data)
-      //.then(() => this.getCampaignResults());
+      .then(data => this.location = data.results.);
 
   }
 
@@ -34,22 +32,15 @@ class MainController {
         self.campaigns = data;
         self.offset += 1;
       })
-      .error( err => console.error(`Error: ${err}`));
-  }
-  getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        this.position = position;
-      });
-    }
+      .error(err => console.error(`Error: ${err}`));
   }
 
   sortCampaignsBy(type) {
-
+    // Sorting Functions
   }
 
   calculateDonations() {
-
+    // Calculate Donations
   }
 
 }
